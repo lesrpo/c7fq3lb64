6191e2744db91d385d5b567afd4a1a7b623e2153
lesrpo <leslierestrepo@gmail.com>
2016-06-27 09:28:07 +0400

    Filters working

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,24 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+  	params.each do |key,value|
+		puts "Param #{key}: #{value}"
+	end
+
+
+	concept = params[:concept]
+
+	category_id = params[:category_id]
+
+	if !concept.blank? && !category_id.blank?
+    	@expenses = Expense.order("date DESC").where("concept LIKE ? AND category_id=?", "%#{concept}%", category_id)
+    elsif concept.blank? && category_id.blank?
+		@expenses = Expense.order("date DESC")
+	elsif category_id.blank?
+		@expenses = Expense.order("date DESC").where("concept LIKE ?", "%#{concept}%")
+	else
+    	@expenses = Expense.order("date DESC").where("category_id=?", category_id)
+    end
+
   end
 end
